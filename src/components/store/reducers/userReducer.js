8a9a32e6f75e86e2a61df84b7d8c26a57310b2c0
brodwen83 <33011691+brodwen83@users.noreply.github.com>
2018-11-14 12:02:43 +0800

    Redux Setup

---
+++ src/components/store/reducers/userReducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  authError: null
+};
+
+const userReducer = (state = initialState, action) => {
+  return state;
+};
+
+export default userReducer;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./components/store/store";
 
 import "semantic-ui-css/semantic.min.css";
 import App from "./components/App";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
